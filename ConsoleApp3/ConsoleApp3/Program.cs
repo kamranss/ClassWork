87b1c029e43bf8f187446ec74310c089d21855c8
Suleymanov Kamran <KamranSS@code.local>
2022-11-13 18:51:46 +0400

    Feature: added Week 3 Classwork

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int b = 7;
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            
+
+            
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+          
+
+
+
+
+        }
+    }
+}
