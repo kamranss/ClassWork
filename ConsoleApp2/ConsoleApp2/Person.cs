f418827e364f019ba05ffca9f309de39406588fc
Suleymanov Kamran <KamranSS@code.local>
2022-11-13 18:46:22 +0400

    Feature: added Week 5 Class lesson

---
+++ ConsoleApp2/ConsoleApp2/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Person
+    {
+        public string Name;
+        public string Surname;
+        public string Phonenumber;
+        public string Category;
+        public string Note;
+
+        public Person()
+        {
+
+        }
+        public Person(string name, string surname, string phonenumber, string category, string note)
+        {
+            Name = name;
+            Surname = surname;
+            Phonenumber = phonenumber;
+            Category = category;
+            Note = note;
+        }
+        public string FullInfo()
+        {
+            return $"{Name} {Surname} {Phonenumber} {Category} {Note}";
+        }
+            
+            
+            
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person per1 = new Person("Kamran", "Suleyman", "+56641564545616", "BackEndDev", "General");
+
+            Console.WriteLine(per1.FullInfo());
+
+        }
+
+
+
+    }
+
+}
