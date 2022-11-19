4405c20a90132ce2c738de4a7fd70d846734a936
Suleymanov Kamran <KamranSS@code.local>
2022-11-19 19:59:31 +0400

    Refactor: updated Week7

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,26 +4,23 @@ namespace ConsoleApp2
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
 
-            Person per1 = new Person("Kamran", "Suleyman", "+56641564545616", "BackEndDev", "General");
-            Console.WriteLine(per1.FullInfo());
+            Car car1 = new Car();
 
-            Person per2 = new Person();
-            per2.Name = "Kamil";
-            per2.Surname = "Agayev";
-            per2.Phonenumber = "+546465467486";
-            per2.Category = "frontEndDev";
-            per2.EmailAddress = "kamilagayev@gmail.com";
+            car1.Model = "djsdkjs";
+
             
 
-            Console.WriteLine(per2.FullInfo());
-
-
-
         }
+        
 
+        
+
+        
 
 
     }
